9b4c98e36d9f3a8d339a64f84d8eeb55bfc6d694
Tomas <tomas.e.gear@gmail.com>
2021-03-28 19:09:57 -0400

    modify procfile to remove space

--- api/app.js
+++ api/app.js
@@ -1,23 +1,23 @@
 const express = require('express')
-var cors = require('cors')
+// var cors = require('cors')
 const app = express()
 const request = require("request"); 
 const port = 5000
 require('dotenv').config()
 
-app.use(cors());
+// app.use(cors());
 
 
 app.get('/', (req, res) => {
 
 
   // https://stackoverflow.com/questions/43871637/no-access-control-allow-origin-header-is-present-on-the-requested-resource-whe
-  console.log("req.params is: ", req.params); 
+  // console.log("req.params is: ", req.params); 
 
-  const obj = {"test": "test"}; 
+  // const obj = {"test": "test"}; 
 
-  res.header("Access-Control-Allow-Origin", "http://localhost:3002");
-  res.send(obj); 
+  // res.header("Access-Control-Allow-Origin", "http://localhost:3002");
+  res.send("Welcome to the API"); 
 
 })
 
@@ -29,7 +29,7 @@ app.get('/fetchConsumerComplaints/:state', (req, res) => {
 
     const { state } = req.params; 
     
-    res.header("Access-Control-Allow-Origin", "http://localhost:3002");
+    // res.header("Access-Control-Allow-Origin", "http://localhost:3002");
     res.redirect(`https://aqueous-atoll-87775.herokuapp.com/https://www.consumerfinance.gov/data-research/consumer-complaints/search/api/v1/geo/states/?${state}`)
 
 })
@@ -40,7 +40,7 @@ app.get('/fetchRobocallComplaints/:state', (req, res) => {
 
     const { state } = req.params; 
     
-    res.header("Access-Control-Allow-Origin", "http://localhost:3002");
+    // res.header("Access-Control-Allow-Origin", "http://localhost:3002");
     res.redirect(`https://aqueous-atoll-87775.herokuapp.com/https://api.ftc.gov//v0/dnc-complaints?api_key=${process.env.API_KEY}&state="${state}"`)
 
 })
