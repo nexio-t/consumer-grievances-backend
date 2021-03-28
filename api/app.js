bc87095fa180cd9e4a5bc9cdd7fce7e7be8344eb
Tomas <tomas.e.gear@gmail.com>
2021-03-28 19:13:57 -0400

    remove cors anywhere prefix

--- api/app.js
+++ api/app.js
@@ -30,7 +30,9 @@ app.get('/fetchConsumerComplaints/:state', (req, res) => {
     const { state } = req.params; 
     
     // res.header("Access-Control-Allow-Origin", "http://localhost:3002");
-    res.redirect(`https://aqueous-atoll-87775.herokuapp.com/https://www.consumerfinance.gov/data-research/consumer-complaints/search/api/v1/geo/states/?${state}`)
+
+    // https://aqueous-atoll-87775.herokuapp.com/
+    res.redirect(`https://www.consumerfinance.gov/data-research/consumer-complaints/search/api/v1/geo/states/?${state}`)
 
 })
 
@@ -39,9 +41,9 @@ app.get('/fetchRobocallComplaints/:state', (req, res) => {
     console.log("req.params is: ", req.params); 
 
     const { state } = req.params; 
-    
+    // https://aqueous-atoll-87775.herokuapp.com/
     // res.header("Access-Control-Allow-Origin", "http://localhost:3002");
-    res.redirect(`https://aqueous-atoll-87775.herokuapp.com/https://api.ftc.gov//v0/dnc-complaints?api_key=${process.env.API_KEY}&state="${state}"`)
+    res.redirect(`https://api.ftc.gov//v0/dnc-complaints?api_key=${process.env.API_KEY}&state="${state}"`)
 
 })
 
